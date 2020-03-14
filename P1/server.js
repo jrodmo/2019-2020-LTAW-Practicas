5220d4b43b44beda57baf3ef3f935f6878db188e
Jorge Rodriguez <jrodmo@MacBook-Air.home>
2020-03-14 19:29:00 +0100

    acabando

--- P1/server.js
+++ P1/server.js
@@ -1,36 +1,45 @@
-//-- Este archivo abre el archivo de solicitud y devuelve el contenido al cliente.
-//-- Puerto donde recibir las peticiones
-const PUERTO = 8080;
-
-//-- Modulo http
 var http = require('http');
-var url=require('url');
-//--modulo fs para lectura de ficheros
-var fs= require('fs');
+var url = require('url');
+var fs = require('fs');
 
 console.log("Arrancando servidor...")
 
 //-- Configurar y lanzar el servidor. Por cada peticion recibida
 //-- se imprime un mensaje en la consola
 http.createServer((req, res) => {
-  console.log("----------> Peticion recibida");
+  console.log("---> Peticion recibida")
+  console.log("Recurso solicitado (URL): " + req.url)
+
+  if (req.url.indexOf("favicon.ico")>0){return;}
+
   var q = url.parse(req.url, true);
-  var filename= "" ;
+  console.log("URL parseada: ")
+  console.log("Host: " + q.host)
+  console.log("pathname:" + q.pathname)
 
-  //-- Obtener fichero a devolver
+  //-- Obtener el fichero. Si es "/" se toma index.html
+  //-- Poner el "." delante para que sean un fichero del directorio actual
+
+  var filename = ""
+
   if (q.pathname == "/")
-    filename += "index.html"
-    else{
-			filename= "." + q.pathname
-		}
+    filename += "/index.html"
+  else {
+    filename = q.pathname
+  }
 
-	tipo= filename.split(".")[1]
-  //-- Leer fichero
+  //-- Obtener el tipo de fichero segun la extension
+  tipo = filename.split(".")[1]
+
+  //-- Obtener el nombre del fichero a partir del recurso solicitado
+  //-- Se añade un . delante
+  filename = "." + filename
+
+  console.log("Filename: " + filename)
+  console.log("Tipo: " + tipo)
+
   fs.readFile(filename, function(err, data) {
-    console.log("Recurso solicitado(URL): " + filename);
-    console.log();
-
-    //-- Fichero no encontrado. Devolver mensaje de error
+    console.log(err);
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
@@ -46,22 +55,12 @@ http.createServer((req, res) => {
     }
 
     //-- Es un css
-    if (tipo == "css"){
+    if (tipo == "css")
       mime = "text/css"
-    }
-
-    if (tipo == "json"){
-      mime = "text/json"
-    }
-
-
     //-- Generar el mensaje de respuesta
     res.writeHead(200, {'Content-Type': mime});
     res.write(data);
     res.end();
   });
 
-}).listen(PUERTO);
-
-console.log("corriendo puerto")
-console.log("Puerto: " + PUERTO)
+}).listen(8080);
